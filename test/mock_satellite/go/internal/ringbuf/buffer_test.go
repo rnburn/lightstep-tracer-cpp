b084a86ff7b4082baa8e417f123509bd3c51998c
Ryan Burn <ryan.burn@gmail.com>
2019-02-23 18:06:00 -0800

    Add additional ringbuf coverage.

--- test/mock_satellite/go/internal/ringbuf/buffer_test.go
+++ test/mock_satellite/go/internal/ringbuf/buffer_test.go
@@ -14,3 +14,16 @@ func TestEmptyBufferProperties(t *testing.T) {
   free_space := buffer.PeekFree()
   require.Equal(t, 10, free_space.Size())
 }
+
+func TestProduceIntoBuffer(t *testing.T) {
+  buffer := NewBuffer(10)
+  free_space := buffer.PeekFree()
+  require.Equal(t, 10, len(free_space.Data1))
+  copy(free_space.Data1, "abc")
+  buffer.Produce(3)
+  contents := buffer.Peek()
+  require.Equal(t, 3, contents.Size())
+  expected_contents := make([]byte, 3)
+  copy(expected_contents, "abc")
+  require.Equal(t, expected_contents, contents.Data1)
+}
